1d39f435636ef47b0253f62681b4fb68dfe5283c
IT PLANT <arrachmansandry@gmail.com>
2024-08-19 08:57:15 +0700

    form 009 od

---
+++ Scripts/page/smf.fm009od.input.js
@@ -0,0 +1,23 @@
+﻿var urlPath = $("#urlPath").val();
+let rteProblem;
+
+$(document).ready(function () {
+    $('#dtTglSs').datepicker({
+        dateFormat: 'yy-M-dd',
+        changeMonth: true,
+        changeYear: true,
+        orientation: "bottom auto"
+    });
+
+    $("#dtTglSs").val(moment().format("YYYY-MMM-DD"))
+    $("#txtTgl").val(moment().format("YYYY-MMM-DD"))
+
+    rteProblem = new Quill('#editor', {
+        modules: {
+            syntax: true,
+            toolbar: '#toolbar-container',
+        },
+        placeholder: 'Compose an epic...',
+        theme: 'snow',
+    });
+})
