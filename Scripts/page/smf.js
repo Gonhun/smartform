6d4b22315cc6a23377cef0a9020b62abae59ab0b
IT PLANT <arrachmansandry@gmail.com>
2024-08-22 14:02:03 +0700

    fix menu

--- Scripts/page/smf.js
+++ Scripts/page/smf.js
@@ -1,18 +1,14 @@
 ﻿var urlPath = $("#urlPath").val();
-
+let menuLength;
 $(document).ready(function () {
-
     getMenu();
 
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-        var options = {
-            damping: '0.5'
-        }
-        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-});
+    const idMenu = document.getElementById("menuForm");
+    const arrayOfData = Array.from(idMenu, node => node.children);
+    console.log(idMenu.getElementsByTagName("li"));
 
+    
+});
 
 
 function getMenu() {
@@ -27,48 +23,44 @@ function getMenu() {
         complete: function (res) {
             document.getElementById("menuForm").innerHTML = "";
 
-            let rowMenu = res.responseJSON.Value;
+            rowMenu = res.responseJSON.Value;
             let rowChild;
-            //console.log(rowMenu);
+
+            menuLength = rowMenu.length;
 
             while (i < rowMenu.length) {
                 rowChild = rowMenu[i].children;
                 document.getElementById("menuForm").innerHTML = document.getElementById("menuForm").innerHTML
-                    + "<li class='nav-item parentMenu'>" +
-                    "<a class='nav-link text-white active bg-gradient-primary' href='#'>" +
-                    "<div class='text-white text-center me-2 d-flex align-items-center justify-content-center'>" +
-                    "<i class='fa fa-sitemap' aria-hidden='true'></i><input type='hidden' value='" + rowMenu[i].id + "' id='txtIdMenu'/>" +
-                    "</div ><span class='nav-link-text ms-1'>" + rowMenu[i].nama + "</span>" +
+                    + "<li class='nav-item'>" +
+                    "<a class='nav-link text-white active bg-gradient-primary parentMenu' onclick='treeLevel(event, "+ [i] +");' href='#' id='master" + [i] + "'>" +
+                        "<div class='text-white text-center me-2 d-flex align-items-center justify-content-center'>" +
+                            "<i class='fa fa-sitemap' aria-hidden='true'></i>" +
+                        "</div ><span class='nav-link-text ms-1'>" + rowMenu[i].nama + "</span>" +
                     "</a>" +
-                    "<ul class='submenu navbar-nav' style='display: block;' id='parent" + rowMenu[i].id + "'>";
-                while (j < rowChild.length) {
-                    document.getElementById("parent" + rowChild[j].parent).innerHTML = document.getElementById("parent" + rowChild[j].parent).innerHTML
-                        + `<li class="nav-item">
-                                <a href="${rowChild[j].domain + rowChild[j].url}" class="nav-link child text-white child" id="form${rowChild[j].id}">
-                                    <div class="text-white text-xs text-center d-flex align-items-center justify-content-center">
-                                    </div>
-                                    <span class="nav-link-text">${rowChild[j].nama}</span>
-                                </a>
-                            </li>`;
-                    var currentUrl = window.location.href;
-                    $('.child').each(function () {
-                        if (this.href === currentUrl) {
-                            $(this).addClass('bg-gradient-danger');
-                            // Ensure the parent submenu is visible
-                            $(this).closest('.submenu').show();
-                            // Add a class to the parent nav-item to keep the submenu open
-                            $(this).closest('.nav-item').addClass('active');
+                    "<ul class='submenu navbar-nav' id='parent"+ [i] +"'>";
+                        while (j < rowChild.length) {
+                            document.getElementById("parent" + [i]).innerHTML = document.getElementById("parent" + [i]).innerHTML
+                            //document.getElementsByClassName("submenu").innerHTML = document.getElementsByClassName("submenu").innerHTML
+                                + `<li class="nav-item">
+                                        <a href="${rowChild[j].domain + rowChild[j].url}" class="nav-link child text-white child" id="form${rowChild[j].id}">
+                                            <div class="text-white text-xs text-center d-flex align-items-center justify-content-center">
+                                            </div>
+                                            <span class="nav-link-text">${rowChild[j].nama}</span>
+                                        </a>
+                                    </li>`;
+                    
+                            j++;
                         }
-                    });
-
-                    j++;
-                }
                 j = 0;
                 "</ul></li>";
                 i++;
             }
-
-
         }
     })
+}
+
+function treeLevel(e, id) {
+    var $yourUl = $("#parent" + id);
+    //$(this).next('.submenu').slidetoggle();
+    $yourUl.css("display", $yourUl.css("display") === 'none' ? 'block' : 'none');
 }
